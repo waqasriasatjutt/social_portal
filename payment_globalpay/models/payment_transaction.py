5d4c196761ac8f22466601d45ed9765b9bae6df8
waqas riasat <waqasriasatjutt@gmail.com>
2024-08-14 11:55:04 +0500

    update

--- payment_globalpay/models/payment_transaction.py
+++ payment_globalpay/models/payment_transaction.py
@@ -2,6 +2,8 @@
 
 import logging
 import pprint
+from odoo.http import request
+
 
 from werkzeug import urls
 
@@ -18,8 +20,9 @@ _logger = logging.getLogger(__name__)
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
+
     def _get_specific_rendering_values(self, processing_values):
-        """ Override of payment to return Mollie-specific rendering values.
+        """ Override of payment to return Global Payments-specific rendering values.
 
         Note: self.ensure_one() from `_get_processing_values`
 
@@ -32,20 +35,49 @@ class PaymentTransaction(models.Model):
             return res
 
         payload = self._globalpay_prepare_payment_request_payload()
-        _logger.info("sending '/payments' request for link creation:\n%s", pprint.pformat(payload))
-        payment_data = self.provider_id._globalpay_make_request('/payments', data=payload)
+        _logger.info("Sending '/payments' request for link creation:\n%s", pprint.pformat(payload))
+        
+        # Example: you may call a function to send a request to Global Payments API here.
+        # payment_data = self.provider_id._globalpay_make_request('/payments', data=payload)
 
-        # The provider reference is set now to allow fetching the payment status after redirection
-        self.provider_reference = payment_data.get('id')
+        # Example: For now, directly using the sandbox URL as you mentioned
+        sandbox_url = 'https://pay.sandbox.realexpayments.com/pay'
 
-        # Extract the checkout URL from the payment data and add it with its query parameters to the
-        # rendering values. Passing the query parameters separately is necessary to prevent them
-        # from being stripped off when redirecting the user to the checkout URL, which can happen
-        # when only one payment method is enabled on Mollie and query parameters are provided.
-        checkout_url = payment_data['_links']['checkout']['href']
-        parsed_url = urls.url_parse(checkout_url)
-        url_params = urls.url_decode(parsed_url.query)
-        return {'api_url': checkout_url, 'url_params': url_params}
+        # Assuming you might want to add some query parameters or additional data to the URL.
+        return {
+            'api_url': sandbox_url,
+            'url_params': {},  # Add any required parameters here
+        }
+
+    # def _get_specific_rendering_values(self, processing_values):
+    #     """ Override of payment to return Mollie-specific rendering values.
+
+    #     Note: self.ensure_one() from `_get_processing_values`
+
+    #     :param dict processing_values: The generic and specific processing values of the transaction
+    #     :return: The dict of provider-specific rendering values
+    #     :rtype: dict
+    #     """
+    #     res = super()._get_specific_rendering_values(processing_values)
+    #     if self.provider_code != 'globalpay':
+    #         return res
+
+    #     payload = self._globalpay_prepare_payment_request_payload()
+    #     _logger.info("sending '/payments' request for link creation:\n%s", pprint.pformat(payload))
+    #     return request.redirect('https://pay.sandbox.realexpayments.com/pay')
+        # payment_data = self.provider_id._globalpay_make_request('/payments', data=payload)
+
+        # # The provider reference is set now to allow fetching the payment status after redirection
+        # self.provider_reference = payment_data.get('id')
+
+        # # Extract the checkout URL from the payment data and add it with its query parameters to the
+        # # rendering values. Passing the query parameters separately is necessary to prevent them
+        # # from being stripped off when redirecting the user to the checkout URL, which can happen
+        # # when only one payment method is enabled on Mollie and query parameters are provided.
+        # checkout_url = payment_data['_links']['checkout']['href']
+        # parsed_url = urls.url_parse(checkout_url)
+        # url_params = urls.url_decode(parsed_url.query)
+        # return {'api_url': checkout_url, 'url_params': url_params}
 
     def _globalpay_prepare_payment_request_payload(self):
         """ Create the payload for the payment request based on the transaction values.
