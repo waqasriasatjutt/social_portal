bec935d67700760d00ba83bce9f42267a84421a1
waqas riasat <waqasriasatjutt@gmail.com>
2024-08-14 10:12:49 +0500

    update

--- payment_globalpay/__manifest__.py
+++ payment_globalpay/__manifest__.py
@@ -13,7 +13,7 @@
         'views/payment_mollie_templates.xml',
         'views/payment_provider_views.xml',
 
-        'data/payment_provider_data.xml',
+        # 'data/payment_provider_data.xml',
     ],
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
