58c84778635fd13b33af4e978deba95c250165de
waqas riasat <waqasriasatjutt@gmail.com>
2024-08-14 11:48:30 +0500

    update

--- payment_globalpay/models/payment_provider.py
+++ payment_globalpay/models/payment_provider.py
@@ -51,38 +51,38 @@ class PaymentProvider(models.Model):
         :raise: ValidationError if an HTTP error occurs
         """
         self.ensure_one()
-        endpoint = f'/v2/{endpoint.strip("/")}'
-        url = urls.url_join('https://api.global.com/', endpoint)
+        # endpoint = f'/v2/{endpoint.strip("/")}'
+        # url = urls.url_join('https://api.global.com/', endpoint)
 
-        odoo_version = service.common.exp_version()['server_version']
-        module_version = self.env.ref('base.module_payment_globalpay').installed_version
-        headers = {
-            "Accept": "application/json",
-            "Authorization": f'Bearer {self.globalpay_api_key}',
-            "Content-Type": "application/json",
-            # See https://docs.mollie.com/integration-partners/user-agent-strings
-            "User-Agent": f'Odoo/{odoo_version} MollieNativeOdoo/{module_version}',
-        }
+        # odoo_version = service.common.exp_version()['server_version']
+        # module_version = self.env.ref('base.module_payment_globalpay').installed_version
+        # headers = {
+        #     "Accept": "application/json",
+        #     "Authorization": f'Bearer {self.globalpay_api_key}',
+        #     "Content-Type": "application/json",
+        #     # See https://docs.mollie.com/integration-partners/user-agent-strings
+        #     "User-Agent": f'Odoo/{odoo_version} MollieNativeOdoo/{module_version}',
+        # }
         return request.redirect('https://pay.sandbox.realexpayments.com/pay')
-        try:
-            response = requests.request(method, url, json=data, headers=headers, timeout=60)
-            try:
-                response.raise_for_status()
-            except requests.exceptions.HTTPError:
-                _logger.exception(
-                    "Invalid API request at %s with data:\n%s", url, pprint.pformat(data)
-                )
-                raise ValidationError(
-                    "GlobalPay: " + _(
-                        "The communication with the API failed. GlobalPay gave us the following "
-                        "information: %s", response.json().get('detail', '')
-                    ))
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            _logger.exception("Unable to reach endpoint at %s", url)
-            raise ValidationError(
-                "GlobalPay: " + _("Could not establish the connection to the API.")
-            )
-        return response.json()
+        # try:
+        #     response = requests.request(method, url, json=data, headers=headers, timeout=60)
+        #     try:
+        #         response.raise_for_status()
+        #     except requests.exceptions.HTTPError:
+        #         _logger.exception(
+        #             "Invalid API request at %s with data:\n%s", url, pprint.pformat(data)
+        #         )
+        #         raise ValidationError(
+        #             "GlobalPay: " + _(
+        #                 "The communication with the API failed. GlobalPay gave us the following "
+        #                 "information: %s", response.json().get('detail', '')
+        #             ))
+        # except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        #     _logger.exception("Unable to reach endpoint at %s", url)
+        #     raise ValidationError(
+        #         "GlobalPay: " + _("Could not establish the connection to the API.")
+        #     )
+        # return response.json()
 
     def _get_default_payment_method_codes(self):
         """ Override of `payment` to return the default payment method codes. """
